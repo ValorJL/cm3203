65139e0719d9432004568e77d40a35b19289df12
yule <carrotsoup@foxmail.com>
2022-04-15 14:50:48 +0000

    Show preview of article

--- src/main/java/com/c2086696/cm3203/Cm3203Application.java
+++ src/main/java/com/c2086696/cm3203/Cm3203Application.java
@@ -2,9 +2,7 @@ package com.c2086696.cm3203;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class Cm3203Application {
 
