a56cb5223f940598d1a58af777b87bfc55564a17
yule <carrotsoup@foxmail.com>
2022-04-15 14:55:04 +0000

    vul branch

--- src/main/java/com/c2086696/cm3203/configuration/securityConfig.java
+++ src/main/java/com/c2086696/cm3203/configuration/securityConfig.java
@@ -9,5 +9,6 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable();
+
     }
 }
